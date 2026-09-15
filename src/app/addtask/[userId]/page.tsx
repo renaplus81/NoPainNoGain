3c603db6802ec1286d2145e3e2cb2d25cce325c7
Rena Takeuchi <takerena0322@icloud.com>
2026-09-15 01:29:57 +0000

    roughly coded addtask_page 0915 10:30

---
+++ src/app/addtask/[userId]/page.tsx
@@ -0,0 +1,92 @@
+import {prisma} from "@/lib/prisma";
+import {redirect} from "next/navigation";
+import Link from"next/link";
+
+//タスクの新規追加
+
+type Props = {
+    params: Promise<{ userId: string }>;
+};
+
+export default function RegisterNewTask({params}:Props){
+
+    async function RegisterTask(formData:FormData){
+        "use server";
+        const { userId } = await params;
+
+        //paramsからとったuserIdがあるので、それを使う。
+        const user_id = Number(userId);
+        //as stringは、ある値が確実に文字列(string)型であることをコンパイラに保証するため
+        const task_name = formData.get("task_name") as string;
+        //Number()を追加、フォーム入力のものは文字列型で返ってくるため
+        const duration = Number(formData.get("duration") as string);
+        const irrational = Number(formData.get("irrational") as string);
+
+
+        const makesureTitle = await prisma.task.findFirst({
+            where:{ 
+                task_name: task_name,
+            },
+        });
+
+        if(makesureTitle){
+            redirect(`/`)
+            //　まだタスク編集画面一覧を作っていないため一旦仮のURL
+        }
+
+        const createTask = await prisma.task.create({
+            data:{ 
+                user_id,
+                task_name,
+                duration,
+                irrational, 
+            },
+        });
+
+        //仮URL
+        redirect(`/`)
+            //　まだタスク編集画面一覧を作っていないため一旦仮のURL
+    }
+
+    return (
+        <div>
+            <Link href="/">
+            {/* まだタスク編集画面一覧を作っていないため一旦仮のURL */}
+            ←タスク一覧画面へ
+            </Link>
+
+            <h1>タスクを新規登録</h1>
+
+            <form action={RegisterTask}> 
+                <div>
+                    <label>タスク名: </label>
+                    <input
+                    name="task_name"
+                    required />
+                </div>
+                <div>
+                    <label>所要時間: </label>
+                    <input
+                    name="duration"
+                    required />
+                </div>
+                <div>
+                    <label>理不尽度合い: </label>
+                    <input
+                    name="irrational"
+                    required />
+                </div>
+
+
+                <div>
+                    <button type="submit">新規登録する</button>
+                </div>
+
+                {/* 仮URL */}
+                {/* <div>
+                    <Link href="/">タスク一覧へ戻る</Link>
+                </div> */}
+            </form>
+        </div>
+    )
+}
